749b32cd26b6ecb38ae08f3083761b1dd460d2ac
kwamboka1 <makorimildred@gmail.com>
2022-04-18 12:05:41 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ holds class User"""
+
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -28,7 +30,7 @@ class User(BaseModel, Base):
         """initializes user"""
         super().__init__(*args, **kwargs)
 
-        def __setattr__(self, name, value):
+    def __setattr__(self, name, value):
         """sets a password with md5 encryption"""
         if name == "password":
             value = md5(value.encode()).hexdigest()
